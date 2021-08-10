12a5281e14b678982b4c34ec405d6912209a0817
Harry-Bro <likeharrybro@gmail.com>
2021-08-11 01:01:30 +0900

    feat: Add dding

--- store/views.py
+++ store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from DDING.settings import get_secret
+
+
 def home(request):
     return render(request, 'main.html')
+
+
+def dding(request):
+    SECRET_KEY = get_secret('KAKAO')
+    return render(request, 'dding.html', {'key': SECRET_KEY})
