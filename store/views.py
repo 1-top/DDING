4c951f46c52753dad0e713552b1afd353aab84ad
Harry-Bro <likeharrybro@gmail.com>
2021-08-11 23:07:15 +0900

    feat: Add save report

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from DDING.settings import get_secret
-from store.models import Report
+from store.models import Report, Category
 
 
 def home(request):
@@ -13,11 +13,13 @@ def dding(request):
     SECRET_KEY = get_secret('KAKAO')
 
     if request.method == 'POST':
-        category = request.POST.get('category', None)
+        _category = request.POST.get('category', None)
         location = request.POST.get('location', None)
         storename = request.POST.get('storename', None)
         latitude = request.POST.get('latitude', None)
         longitude = request.POST.get('longitude', None)
-        print(category, location, storename, latitude, longitude)
+        category, _ = Category.objects.get_or_create(name=_category)
+        if category and location and storename and latitude and longitude:
+            Report(category=category, location=location, name=storename, latitude=latitude, longitude=longitude).save()
 
     return render(request, 'dding.html', {'key': SECRET_KEY})
